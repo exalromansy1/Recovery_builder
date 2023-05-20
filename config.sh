42eab3fe8f1e7108634308ef912a0bc28efd5410
exalromansy1 <120054209+exalromansy1@users.noreply.github.com>
2023-05-21 00:27:58 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,12 +6,12 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="Infinix-X626B" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X626B.git" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="Infinix" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="omni_Infinix-X626B.mk" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
